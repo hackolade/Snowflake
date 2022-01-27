2c6aff728175526bdcd631a440472ab01a0a5ab6
DrMyroslav <myroslav.druchkiv@binary-studio.com>
2022-01-27 15:01:30 +0200

    change tab

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -10,20 +10,20 @@ module.exports = {
 		callback(null, '');
 	},
 
-  applyToInstance(connectionInfo, logger, callback, app) {
-    const { applyToInstance } = require('./helpers/applyToInstanceHelper');
+	applyToInstance(connectionInfo, logger, callback, app) {
+		const { applyToInstance } = require('./helpers/applyToInstanceHelper');
 
-    applyToInstance(connectionInfo, logger, app)
-      .then(result => {
-        callback(null, result);
-      })
-      .catch(error => {
-        const err = {
-          message: error.message,
-          stack: error.stack,
-        };
-        logger.log('error', err, 'Error when applying to instance');
-        callback(err);
-      });
-  },
+		applyToInstance(connectionInfo, logger, app)
+			.then(result => {
+				callback(null, result);
+			})
+			.catch(error => {
+				const err = {
+					message: error.message,
+					stack: error.stack,
+				};
+				logger.log('error', err, 'Error when applying to instance');
+				callback(err);
+			});
+	},
 };
