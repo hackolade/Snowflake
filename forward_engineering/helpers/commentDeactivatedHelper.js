3cc5777857130c34621bf0c5437e7ff7e1b51166
oleg_chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2023-11-14 11:27:33 +0200

    fix: added missing functions

--- forward_engineering/helpers/commentDeactivatedHelper.js
+++ forward_engineering/helpers/commentDeactivatedHelper.js
@@ -1,3 +1,6 @@
+const MULTILINE_COMMENT = /(\n\/\*\n[\s\S]*?\n\s\*\/\n)|(\n\/\*\n[\s\S]*?\n\s\*\/)$/gi;
+const STARTS_QUERY = ['//', '--'];
+
 module.exports = (_) => {
 	const commentIfDeactivated = (statement, data, isPartOfLine) => {
 		if (_.has(data, 'isActivated') && !data.isActivated) {
@@ -12,7 +15,13 @@ module.exports = (_) => {
 		return statement;
 	};
 
+	const filterDeactivatedQuery = query => query.replace(MULTILINE_COMMENT, '');
+
+	const queryIsDeactivated = (query = '') => STARTS_QUERY.some(startQuery => query.startsWith(startQuery));
+
 	return {
 		commentIfDeactivated,
+		filterDeactivatedQuery,
+		queryIsDeactivated
 	};
 };
