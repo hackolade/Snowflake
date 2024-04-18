6d62ae38207638cba83e2b3ad01c9ee28dcead14
Serhii Filonenko <91055067+serhii-filonenko@users.noreply.github.com>
2024-04-18 13:14:30 +0300

    FE: fix obtain composite keys from json schema (#111)

--- forward_engineering/helpers/keyHelper.js
+++ forward_engineering/helpers/keyHelper.js
@@ -108,7 +108,7 @@ module.exports = app => {
 			}));
 	};
 
-	const getTableKeyConstraints = jsonSchema => {
+	const getTableKeyConstraints = ({ jsonSchema }) => {
 		if (!jsonSchema.properties) {
 			return [];
 		}
