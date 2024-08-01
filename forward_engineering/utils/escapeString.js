3589b4563222676f1f7d612e1c20a596e06c3381
yevhenii-moroziuk <yevhen.moroziuk@hackolade.com>
2024-08-01 13:00:55 +0300

    HCK-7477: update regex

--- forward_engineering/utils/escapeString.js
+++ forward_engineering/utils/escapeString.js
@@ -1,7 +1,7 @@
 const { FORMATS } = require('../helpers/constants');
 
 const MUST_BE_ESCAPED =
-	/(\\r|\\n|\\b|\\f|\\t|'|"|\\0|\[0-9]{3}|\x5C[0-3]?[0-7]{1,2}|\x5C[xX][0-9A-Fa-f]{2}|\x5C[uU][0-9A-Fa-f]{4})/g;
+	/(\\r|\\n|\\b|\\f|\\t|'|"|\\[0-9]{3}|\0|\\x5C[0-3]?[0-7]{1,2}|\x5C[xX][0-9A-Fa-f]{2}|\x5C[uU][0-9A-Fa-f]{4})/g;
 
 const escapeString = (scriptFormat, value) =>
 	scriptFormat === FORMATS.SNOWSIGHT ? `'${value.replace(MUST_BE_ESCAPED, '\\$1')}'` : `$$${value}$$`;
