785ab01b4cbb55ce5a9e6a061fac271fd8d9dc52
Yevhenii Moroziuk <yevhen.moroziuk@hackolade.com>
2024-08-01 14:19:10 +0300

    HCK-7477 (#126)
    * HCK-7477: Update regex
    
    * HCK-7477: Fix tests
    
    * HCK-7477: update regex

--- forward_engineering/helpers/commentHelpers/commentDeactivatedHelper.js
+++ forward_engineering/helpers/commentHelpers/commentDeactivatedHelper.js
@@ -1,7 +1,6 @@
 const _ = require('lodash');
 
-const MULTILINE_COMMENT = /(\n\/\*\n[\s\S]*?\n\s\*\/\n)|(\n\/\*\n[\s\S]*?\n\s\*\/)$/gi;
-const STARTS_QUERY = ['//', '--'];
+const STARTS_QUERY = ['//'];
 
 const commentIfDeactivated = (statement, data, isPartOfLine) => {
 	if (_.has(data, 'isActivated') && !data.isActivated) {
@@ -22,12 +21,9 @@ const commentIfDeactivated = (statement, data, isPartOfLine) => {
 	return statement;
 };
 
-const filterDeactivatedQuery = query => query.replace(MULTILINE_COMMENT, '');
-
 const queryIsDeactivated = (query = '') => STARTS_QUERY.some(startQuery => query.startsWith(startQuery));
 
 module.exports = {
 	commentIfDeactivated,
-	filterDeactivatedQuery,
 	queryIsDeactivated,
 };
--- forward_engineering/helpers/commentHelpers/commentDropStatements.js
+++ forward_engineering/helpers/commentHelpers/commentDropStatements.js
@@ -3,7 +3,6 @@ const { DROP_STATEMENTS } = require('../constants');
 const commentDropStatements = (script = '') =>
 	script
 		.split('\n')
-		.filter(line => line.trim())
 		.map(line => {
 			if (DROP_STATEMENTS.some(statement => line.includes(statement))) {
 				return `// ${line}`;
