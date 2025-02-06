1d9bf21c7a0de3033d31b11a04e8631f974f7f1c
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2025-02-06 18:10:44 +0200

    HCK-9791: add types

--- forward_engineering/dbtProvider.js
+++ forward_engineering/dbtProvider.js
@@ -1,7 +1,15 @@
+/**
+ * @typedef {import('./types').ColumnDefinition} ColumnDefinition
+ */
+
 module.exports = ({ app }) => {
 	const { decorateType } = require('./helpers/columnDefinitionHelper')(app);
 
 	return {
+		/**
+		 * @param {{ columnDefinition: ColumnDefinition; }}
+		 * @returns {{ type: string; }}
+		 */
 		getColumnDataTypeData({ columnDefinition }) {
 			return {
 				type: decorateType(columnDefinition.type, columnDefinition),
