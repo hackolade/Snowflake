d4312cff8b9dd8727ff14c1e3f79dd1ef91012e4
Nazar Kovtun <88377450+WilhelmWesser@users.noreply.github.com>
2025-08-14 15:49:52 +0300

    HCK-12309: changed error message in case of invalid username provided (#207)

--- common/getKeyPairConnectionErrorMessageByCode.js
+++ common/getKeyPairConnectionErrorMessageByCode.js
@@ -2,9 +2,9 @@ const errorMessages = require('./errorMessages.js');
 const errorCodes = require('./errorCodes.js');
 
 const ERROR_CODE_TO_MESSAGE = {
-	[errorCodes.ERR_MISSING_PASSPHRASE]: errorMessages.KEY_PAIR_ERROR,
-	[errorCodes.ERR_OSSL_BAD_DECRYPT]: errorMessages.KEY_PAIR_ERROR,
-	[errorCodes.ERR_INVALID_USERNAME]: errorMessages.KEY_PAIR_ERROR,
+	[errorCodes.ERR_MISSING_PASSPHRASE]: errorMessages.KEY_PAIR_PASSPHRASE_ERROR,
+	[errorCodes.ERR_OSSL_BAD_DECRYPT]: errorMessages.KEY_PAIR_PASSPHRASE_ERROR,
+	[errorCodes.ERR_INVALID_USERNAME]: errorMessages.KEY_PAIR_USERNAME_ERROR,
 };
 
 const getKeyPairConnectionErrorMessageByCode = code => ERROR_CODE_TO_MESSAGE[code];
