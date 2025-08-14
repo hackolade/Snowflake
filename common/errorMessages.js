0b0b96b75917682c6348ed6267e54db891f2351c
Nazar Kovtun <nazar.kovtun@hackolade.com>
2025-08-14 13:06:39 +0300

    HCK-12309: changed error message in case of invalid username provided

--- common/errorMessages.js
+++ common/errorMessages.js
@@ -5,5 +5,6 @@ module.exports = {
 		'Incorrect Okta username/password or MFA is enabled. Please, check credentials or use the "Identity Provider SSO (via external browser)" for MFA auth',
 	OKTA_MFA_ERROR:
 		'Native Okta auth doesn\'t support MFA. Please, use the "Identity Provider SSO (via external browser)" auth instead',
-	KEY_PAIR_ERROR: 'Please check that the passphrase is provided and matches the key.',
+	KEY_PAIR_PASSPHRASE_ERROR: 'Please check that the passphrase is provided and matches the key.',
+	KEY_PAIR_USERNAME_ERROR: 'Incorrect username was specified.',
 };
