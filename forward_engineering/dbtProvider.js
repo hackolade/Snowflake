119cd89a3f6a8b2fc9bfb48d967e7ac8d2c56066
Vitalii Yarmus <mr.yarmus@gmail.com>
2026-04-21 17:37:54 +0300

    HCK-15809: extend dbtProvider to support certain config options

--- forward_engineering/dbtProvider.js
+++ forward_engineering/dbtProvider.js
@@ -86,6 +86,17 @@ class DbtProvider {
 
 		return keyHelper.getColumnConstraints({ columnDefinition, jsonSchema });
 	}
+
+	/**
+	 * @param {{ modelData: object[]; containerData: object[]; entityData: object[];}}
+	 * @returns {{ databaseName?: string, schemaName?: string }}
+	 */
+	getEntityProperties({ modelData, containerData, entityData }) {
+		return {
+			databaseName: containerData?.[0]?.database,
+			schemaName: containerData?.[0]?.code ?? containerData?.[0]?.name,
+		};
+	}
 }
 
 module.exports = DbtProvider;
